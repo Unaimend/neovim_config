7de703077d5a9d86d796f716a451592d628f7332
Thomas Dost@NixOS <Unaimend@gmail.com>
2025-11-15 20:10:58 +0100

    Added debug commands

--- init.lua
+++ init.lua
@@ -19,13 +19,21 @@ vim.keymap.set('n', '<leader>bh', '<cmd>nohlsearch<CR>')
 -- Switch to last buffer on <leader>tab
 vim.keymap.set('n', '<leader><Tab>', '<C-^>', { noremap = true, desc = "Switch to last buffer" })
     
+-- Execute current file
+vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "Reload current file (config/settings)" })
+--execite curent line
+vim.keymap.set("n", "<leader>x", ":.lua<CR>", { desc = "Enter Lua command in Normal mode" })
+vim.keymap.set("v", "<leader>x", ":.lua<CR>", { desc = "Enter Lua command in Visual mode" })
 
--- TELESCOPE 
+-- TELESCOPE
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set("n", "<leader>fd", "<CMD>Telescope lsp_definitions<CR>")
+vim.keymap.set("n", "<leader>fd", "<CMD>Telescope lsp_document_symbols<CR>")
+
 vim.keymap.set("n", "<leader>fc", function()
   require('telescope.builtin').find_files {
     cwd = vim.fn.stdpath("config")
