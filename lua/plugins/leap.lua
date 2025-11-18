86baac1a47181eecdfa40d5781c520ae1d2c664b
Thomas Dost@NixOS <Unaimend@gmail.com>
2025-11-18 20:22:20 +0100

    Reenabled leap

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -2,10 +2,9 @@
 
 return {
   'ggandor/leap.nvim',
-  enabled = false,
+  enabled = true,
   -- Recommended default mappings
    config = function()
-     require('leap').add_default_mappings(false) -- Disable default mappings
      vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
      vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
    end
